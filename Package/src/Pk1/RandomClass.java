02072d6da334a0ec5700fca6e13e15ebc28d1647
chugaumuaxuan <thang.doan@ntq-solution.com.vn>
2022-01-25 17:53:44 +0700

    first commit

---
+++ Package/src/Pk1/RandomClass.java
@@ -0,0 +1,13 @@
+package Pk1;
+
+public class RandomClass {
+	
+	   public void Foo1(){
+		   System.out.println("abc");
+	   }
+	   
+	   public void Foo2() {
+		   System.out.println("123");
+	   }
+}
+	    
---
+++ Package/src/main.java
@@ -0,0 +1,16 @@
+import Pk1.RandomClass;
+
+public class main {
+
+	
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		RandomClass tuoi = new RandomClass(); 
+		tuoi.Foo1();
+		tuoi.Foo2();
+		
+	}
+
+}
